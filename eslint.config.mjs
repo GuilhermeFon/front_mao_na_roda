abed7a58b50443fa35906500fb64e5a9b0849d5a
Guilherme Fonseca <guilhermelimafonseca@gmail.com>
2024-12-10 00:27:56 -0300

    refactor eslint configuration to use updated TypeScript parser and plugins; improve structure and readability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,32 @@
 import pluginJs from '@eslint/js';
 import importPlugin from 'eslint-plugin-import';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    plugins: {
-      import: importPlugin,
-    },
-    rules: {
-      'import/order': [
-        'error',
-        {
-          groups: [['builtin', 'external', 'internal']],
-          'newlines-between': 'always',
-          alphabetize: { order: 'asc', caseInsensitive: true },
-        },
-      ],
-    },
+/** @type {import('eslint').Linter.Config} */
+export default {
+  files: ['**/*.{js,mjs,cjs,ts}'],
+  languageOptions: {
+    globals: globals.browser,
+    parser: tsParser,
   },
-];
+  plugins: {
+    import: importPlugin,
+    '@typescript-eslint': tseslint,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [['builtin', 'external', 'internal']],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+  },
+};
